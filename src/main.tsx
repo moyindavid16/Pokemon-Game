6dfc61ce02495eb8e9c525123cc90440f475058f
moyin <moyindavid16@gmail.com>
2022-10-12 21:29:16 +0100

    Game Complete
    Completed game mechanics with no styling

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Game from './pages/Game'
-import HowToPlay from './pages/HowToPlay'
-import MainMenu from './pages/MainMenu'
-import './main.css'
-import Background from './Background'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Game from "./pages/Game";
+import HowToPlay from "./pages/HowToPlay";
+import MainMenu from "./pages/MainMenu";
+import "./main.css";
+import Background from "./Background";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+export const client = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Background />
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<MainMenu />} />
-        <Route path='/play' element={<Game />} />
-        <Route path='/how-to-play' element={<HowToPlay />} />
-      </Routes>
-    </BrowserRouter>
-    
+    {/*<Background />*/}
+    <QueryClientProvider client={client}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainMenu />} />
+          <Route path="/play" element={<Game />} />
+          <Route path="/how-to-play" element={<HowToPlay />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>
-)
+);
--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,7 +1,69 @@
-export default function Game(){
-    return(
-        <>
-            Game page
-        </>
-    )
+import { useQuery } from "@tanstack/react-query";
+import { Key, useMemo, useRef, useState } from "react";
+import Background from "../Background";
+import {client} from "../main"
+
+export default function Game() {
+  const [answerStatus, setAnswerStatus] = useState<undefined | Result>(
+    undefined
+  );
+  const pokeArray = useRef<Key[]>([]);
+  
+  const randomPokeFetcher = () => {
+    let id = Math.floor(Math.random() * 905) + 1;
+    return fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.name);
+        pokeArray.current = [];
+        return data;
+      });
+  };
+
+  const handleAnswerClick = (userResponse: Key) => {
+    setAnswerStatus(userResponse == poke1.name ? Result.Correct : Result.Wrong);
+  };
+
+  const {
+    isLoading: poke1IsLoading,
+    data: poke1,
+    //refetch,
+  } = useQuery(["randomPokemon1"], randomPokeFetcher);
+  const { isLoading: poke2IsLoading, data: poke2 } = useQuery(
+    ["randomPokemon2"],
+    randomPokeFetcher
+  );
+  const { isLoading: poke3IsLoading, data: poke3 } = useQuery(
+    ["randomPokemon3"],
+    randomPokeFetcher
+  );
+
+  const refetch = async () => {
+    await client.refetchQueries();
+  }
+
+  enum Result {
+    Correct,
+    Wrong,
+  }
+
+  if (poke1IsLoading || poke2IsLoading || poke3IsLoading)
+    return <div>Loading...</div>;
+if(pokeArray.current.length==0)pokeArray.current=[poke1.name,poke2.name,poke3.name].sort(()=> 0.5-Math.random())
+    
+
+  return (
+    <>
+      Game page
+      <img src={poke1.sprites.front_default} alt="" />
+      {pokeArray.current.map((pokeName) => (
+        <button key={pokeName} onClick={() => handleAnswerClick(pokeName)}>
+          {pokeName}
+        </button>
+      ))}
+      {answerStatus == Result.Correct && <div>Correct</div>}
+      {answerStatus == Result.Wrong && <div>Wrong</div>}
+      <button onClick={() => refetch()}>Play again</button>
+    </>
+  );
 }
